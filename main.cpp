f44933b911262fad6208613dcdcf3cb9cd372e4a
timerns <tim.ernst07.01@gmail.com>
2022-01-14 13:32:56 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : main.cpp
+Nom du labo    :
+Auteur(s)      : Tim Ernst
+Date creation  :
+Description    :
+Remarque(s)    :
+Compilateur    : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+using namespace std;
+int main() {11
+
+
+
+   cout << "Veillez presser ENTRER pour quitter" << endl;
+   cin.ignorenumeric_limits<streamsize>::max(), '\n');
+
+   return EXIT_SUCCESS;
+}
